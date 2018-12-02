07e002ae132a8cb3a54e8b03c7d9616b6590ee18
apady <frankchenls@outlook.com>
2018-12-02 16:32:25 +0800

    Improve build script

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ set -x -e
 ########################################
 # build php extension for bfs-php-sdk
 ########################################
+
+SRC_DIR=`pwd`/src
+
+cd ${SRC_DIR}
+
 phpize
 
 ./configure
 
 sudo make && make install
+
+cd -
