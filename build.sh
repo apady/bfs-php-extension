b1667e28da561dc9d694b89ddf7c90955e0fa6dc
apady <frankchenls@outlook.com>
2019-01-04 12:34:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,10 @@ set -x -e
 ########################################
 
 SRC_DIR=`pwd`/src
-
 cd ${SRC_DIR}
-
 phpize
-
 ./configure
-
 sudo make && make install
-
+touch /etc/php.d/bfs.ini
+echo -e "extension=bfs.so" > /etc/php.d/bfs.ini
 cd -
